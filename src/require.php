16153624f556620c492a46e5f0c73f1b7658d092
yidicala <paradox0504@gmail.com>
2015-03-24 09:57:03 +0800

    update

--- src/require.php
+++ src/require.php
@@ -2,7 +2,13 @@
 
 // 定义SRC根目录
 define('SRCPATH', dirname( __FILE__ ));
+//auto_load
 require_once SRCPATH . '/auto_load.php';
-require_once SRCPATH . '/data/pb_proto_tencent.crm.spkey.php';
+// //pb body file
+// require_once SRCPATH . '/data/pb_proto_tencent.crm.spkey.php';
+// //pb head file
+// require_once SRCPATH . '/data/pb_proto_tencent.crm.test.head.php';
+//udp server
 require_once SRCPATH . '/serv/testUdpServ.php';
-
+// //weblib
+// require_once SRCPATH . '/weblib/require.php';
